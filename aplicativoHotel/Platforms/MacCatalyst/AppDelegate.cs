b14735a24bee738300c57c4de4a792955625837f
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-04 10:48:28 -0300

    Adicionar arquivos de projeto.

---
+++ aplicativoHotel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace aplicativoHotel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
