5aaabdb0dcd756dc75f6011c089512a9a831134b
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-09 17:15:07 -0300

    criando classe quarto

---
+++ aplicativoHotel/Models/Quarto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplicativoHotel.Models
+{
+    public class Quarto
+    {
+
+
+        public string Descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca{ get; set; }
+
+    }
+}
