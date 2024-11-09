f56521ece0354244f10e3eae96296d52e2a5e99d
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-09 17:10:05 -0300

    try e catch do hospedagemContratada

--- aplicativoHotel/Views/hospedagemContratada.xaml.cs
+++ aplicativoHotel/Views/hospedagemContratada.xaml.cs
@@ -9,6 +9,16 @@ public partial class hospedagemContratada : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
+        try
+        {
 
+            Navigation.PopAsync(); //permite trocar de tela (voltando)
+
+        }
+        catch (Exception ex)
+        {
+
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
