da3b31d333431114e8ca6689441192f30418919a
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-09 17:19:17 -0300

    chamando class

--- aplicativoHotel/App.xaml.cs
+++ aplicativoHotel/App.xaml.cs
@@ -1,8 +1,37 @@
 ﻿
+using aplicativoHotel.Models;
+
 namespace aplicativoHotel
 {
     public partial class App : Application
     {
+         public List<Quarto> lista_quartos = new List<Quarto>
+         {
+             new Quarto()
+             {
+
+                 Descricao= "Suite Luxo",
+                 ValorDiariaAdulto = 110.0,
+                 ValorDiariaCrianca = 55.0
+             },
+               new Quarto()
+             {
+
+                 Descricao= "Suite Single",
+                 ValorDiariaAdulto = 114.0,
+                 ValorDiariaCrianca = .0
+             },
+               new Quarto()
+             {
+
+                 Descricao= "Suite Crise",
+                 ValorDiariaAdulto = 25,
+                 ValorDiariaCrianca = 12
+             }
+
+
+         };
+
         public App()
         {
             InitializeComponent();
