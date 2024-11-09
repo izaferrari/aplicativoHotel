e8a1c7fd4c81cdf17dd54c0364c53b59bdbde6c2
Izabely Ferrari <belymiaferrari@gmail.com>
2024-11-09 17:35:06 -0300

    cs do contratacaoDaHospedagem

--- aplicativoHotel/Views/contratacaoDaHospedagem.xaml.cs
+++ aplicativoHotel/Views/contratacaoDaHospedagem.xaml.cs
@@ -12,6 +12,11 @@ public partial class contratacaoDaHospedagem : ContentPage
 		PropriedadesApp = (App)Application.Current;
 
 		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkout.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month +1, DateTime.Now.Day);
+		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+		dtpck_checkout.MaximumDate = dtpck_checkin.Date.Date.AddMonths(6);
 	}
 
     private void Button_Clicked(object sender, EventArgs e)
@@ -25,5 +30,14 @@ public partial class contratacaoDaHospedagem : ContentPage
 
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker elemento = sender as DatePicker;
+		DateTime data_selecionada_checkin = elemento.Date;
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.Date.AddMonths(6);
+
     }
 }
